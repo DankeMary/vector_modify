6b47caf7607645f41db33361fad74f0ba4b63643
DankeMary <laktionofs@mail.ru>
2017-03-25 20:33:40 +0300

    Added function "findNumDifference"

--- Vector_Modify/Vector_Modify.cpp
+++ Vector_Modify/Vector_Modify.cpp
@@ -1,9 +1,29 @@
 // Vector_Modify.cpp: определяет точку входа для консольного приложения.
-//
+//18. Заменить каждое четное число на разность максимального и минимального чисел. { A) вектор (std::vector)}
+// Выполнила : Лактионова Мария Александровна (2к, 9г)
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 
+int findNumDifference(std::vector<int> v)
+{
+	int min = 0;
+	int max = 0;
+	for (unsigned i = 0; i < v.size(); ++i)
+	{
+		if (v[i] < min) {
+			min = v[i];
+		}
+		else if (v[i] > max) {
+			max = v[i];
+		}
+	}
+	return (max - min);
+}
 int main()
 {
     return 0;
